3b5b639db8b3a2d71a60d1e5d7fd9c06f28545de
Phillipe Moreira <phillipealexmoreira@gmail.com>
2016-12-07 21:01:00 -0200

    Day 23: BST level traversal order

---
+++ C#/Tutorials/30DaysOfCode/Day23_BST_LevelTraversalOrder.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HackerRank.BST
+{
+    class Node
+    {
+        public Node left, right;
+        public int data;
+        public Node (int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    /// <summary>
+    /// Problem link:https://www.hackerrank.com/challenges/30-binary-trees
+    /// </summary>
+    public class Day23_BST_LevelTraversalOrder
+    {
+        static void levelOrder(Node root)
+        {
+            Queue<Node> bstQueue = new Queue<Node>();
+
+            bstQueue.Enqueue(root);
+            while (bstQueue.Count > 0)
+            {
+                Node cur = bstQueue.Dequeue();
+                Console.Write(cur.data + " ");
+
+                if (cur.left != null)
+                {
+                    bstQueue.Enqueue(cur.left);
+                }
+
+                if (cur.right != null)
+                {
+                    bstQueue.Enqueue(cur.right);
+                }
+            }
+        }
+
+        static Node insert(Node root, int data)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node cur;
+                if (data <= root.data)
+                {
+                    cur = insert(root.left, data);
+                    root.left = cur;
+                }
+                else
+                {
+                    cur = insert(root.right, data);
+                    root.right = cur;
+                }
+                return root;
+            }
+        }
+
+        public static void Main()
+        {
+            Node root = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                root = insert(root, data);
+            }
+
+            levelOrder(root);
+        }
+    }
+}
